dbd72c795ecc24927cce368ba933fccb7a2493d1
Maxim V Filimonov <part1zancheg@gmail.com>
2012-07-18 15:11:10 +0400

    a slight fix in main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import os,sys,time
 
 scripts_to_run = []
 
+search_string = 'find . -maxdepth 1 -type f -name "test-*.py" '
+for script in ['test-feedback.py', 'test-settings.py', 'test-contacts.py']:
+	search_string = search_string + ' -and -not -name "'+script+'"'
+
 if len(sys.argv) < 2:
-	scripts_to_run = [script.rstrip() for script in os.popen('find . -maxdepth 1 -type f -name "test-*.py" -and -not -name "test-feedback.py" -and -not -name "test-settings.py" -and -not -name "test-contacts.py"').readlines()]
+	scripts_to_run = [script.rstrip() for script in os.popen(search_string).readlines()]
+	#	scripts_to_run = [script.rstrip() for script in os.popen('find . -maxdepth 1 -type f -name "test-*.py" -and -not -name "test-feedback.py" -and -not -name "test-settings.py" -and -not -name "test-contacts.py"').readlines()]
 else:
 	scripts_to_run = ['./'+sys.argv[index] for index in range(1, len(sys.argv))]
 
