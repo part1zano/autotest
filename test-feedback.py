496ca1c8457afd342ed7be8435ce68415934a3e9
Maxim V Filimonov <part1zancheg@gmail.com>
2012-08-07 16:43:55 +0400

    Added a negative testcase into test-feedback
    Enter empty msg, see what happens
     Changes to be committed:
    	modified:   test-feedback.py

--- test-feedback.py
+++ test-feedback.py
@@ -8,9 +8,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException,NoSuchElementException,WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 
-objfile = codecs.open('objlists/edit-contacts/text/objlist-edit-contacts.conf', encoding='utf-8')
-objlist = objfile.readlines()
-objfile.close()
+msgs = ['This is just a test msg', '']
 
 log = logger.Log('tests.conf')
 
@@ -38,44 +36,64 @@ if not functions.find_link_and_click(driver, u'Обратная связь', 'fe
 
 log.write('info', 'feedback link found and clicked')
 
-if not functions.check_div(driver, 'feedback'):
-	log.write('error', 'no feedback div')
-	driver.close()
-	sys.exit(1)
-
-log.write('info', 'feedback div found')
-
-if not functions.edit_control(driver, 'feedback-message', 'This is just a test msg', 'text'):
-	log.write('error', 'failed to edit text area')
-	driver.close()
-	sys.exit(1)
-
-log.write('info', 'edited message text')
-
-if login not in functions.get_value(driver, 'feedback-email'):
-	log.write('error', 'email not stated in email field')
-	driver.close()
-	sys.exit(1)
-
-try:
-	driver.find_element_by_partial_link_text(u'Отправить').click()
-except NoSuchElementException:
-	log.write('error', 'no submit button or wrong link text')
-	driver.close()
-	sys.exit(1)
-
-log.write('info', 'submit clicked')
-
-for text in ['OK', u'ОК']:
-	try:
-		driver.find_element_by_partial_link_text(text).click()
-		log.write('info', 'ok on informer clicked')
-		log.write('info', 'test PASSED')
+for msg in msgs:
+	if not functions.check_div(driver, 'feedback'):
+		log.write('error', 'no feedback div')
 		driver.close()
-		sys.exit(0)
-	except NoSuchElementException:
-		log.write('warning', 'no '+text+' button')
+		sys.exit(1)
 
-log.write('error', 'no ok button on informer or no informer')
+	log.write('info', 'feedback div found')
+	
+	if not functions.edit_control(driver, 'feedback-message', msg, 'text'):
+		log.write('error', 'failed to edit text area')
+		driver.close()
+		sys.exit(1)
+
+	log.write('info', 'edited message text')
+
+	if login not in functions.get_value(driver, 'feedback-email'):
+		log.write('error', 'email not stated in email field')
+		driver.close()
+		sys.exit(1)
+	
+	try:
+		driver.find_element_by_partial_link_text(u'Отправить').click()
+	except NoSuchElementException:
+		log.write('error', 'no submit button or wrong link text')
+		driver.close()
+		sys.exit(1)
+
+	log.write('info', 'submit clicked')
+	
+	if bool(msg):
+		for text in ['OK', u'ОК']:
+			try:
+				ok = driver.find_element_by_partial_link_text(text)
+			except NoSuchElementException:
+				log.write('warning', 'no '+text+' button')
+
+		try:
+			ok.click()
+		except NameError:
+			log.write('error', 'no ok button on informer or no informer')
+			driver.close()
+			sys.exit(1)
+	else:
+		try:
+			info = driver.find_element_by_name('error-text')
+		except NoSuchElementException:
+			log.write('error', 'no error informer')
+			driver.close()
+			sys.exit(1)
+
+		if u'не должно быть пустым' not in info.get_attribute('value'):
+			log.write('error', 'error text wrong')
+			log.write('error', 'it should contain an msg bout not empty, but doesnt')
+			driver.close()
+			sys.exit(1)
+
+	driver.get(driver.current_url)
+
+log.write('info', 'test PASSED')
 driver.close()
-sys.exit(1)
+sys.exit(0)
