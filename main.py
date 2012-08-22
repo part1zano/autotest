36f1592acfd22e48cd4378af2436c68153ae697f
Maxim V Filimonov <part1zancheg@gmail.com>
2012-08-22 12:01:00 +0400

    Fxd main.py for new filenames
    Made a dog-nail for not reconfiguring Jenkins job
     Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os,sys,time
+import os,sys,time,re
 
 scripts_to_run = []
 
@@ -18,7 +18,7 @@ if len(sys.argv) < 2:
 	scripts_to_run = [script.rstrip() for script in os.popen(search_string).readlines()]
 	#	scripts_to_run = [script.rstrip() for script in os.popen('find . -maxdepth 1 -type f -name "test-*.py" -and -not -name "test-feedback.py" -and -not -name "test-settings.py" -and -not -name "test-contacts.py"').readlines()]
 else:
-	scripts_to_run = ['./'+sys.argv[index] for index in range(1, len(sys.argv))]
+	scripts_to_run = ['./'+re.sub('-', '_', sys.argv[index]) for index in range(1, len(sys.argv))]
 
 fails = 0
 
