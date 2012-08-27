0b00ed65c493ca5491fbe42712efc70596027e55
Maxim V Filimonov <part1zancheg@gmail.com>
2012-08-27 16:32:53 +0400

    Added test for links in settings
    Aaaaand it works!
     On branch experimental
     Changes to be committed:
    	new file:   json_lists/settings-links/links.json
    	new file:   test_settings_links.py

---
+++ test_settings_links.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+from testlib import testcase
+import sys,re
+
+class TestCase(testcase.TestObject):
+	def __init__(self, config='tests.conf'):
+		testcase.TestObject.__init__(self, config)
+
+		self.links = self.make_json_list('json_lists/settings-links/links.json')
+
+	def execute(self):
+		if not testcase.TestObject.execute(self):
+			self.log.write('error', 'login failed')
+			return False
+
+		for link in self.links:
+			if not self.visit_dlink(link):
+				self.log.write('error', 'error visiting '+link['url']+', see above')
+				return False
+
+			divid = re.sub('-', '_', link['url'])
+
+			if not self.check_div(divid):
+				self.log.write('error', 'div id='+divid+' not found')
+				return False
+
+		return True
+
+if __name__ == '__main__':
+	tc = TestCase()
+	sys.exit(int(not tc.execute()))
+#	if not tc.execute():
+#		tc.log.write('error', 'test FAILED, see above')
+#		sys.exit(1)
+#
+#	tc.log.write('info', 'test PASSED')
+#	sys.exit(0)
