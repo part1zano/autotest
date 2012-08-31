ba08cff514c46e6af9ffdb853b5eb85133fccfb9
Maxim V Filimonov <part1zancheg@gmail.com>
2012-08-31 18:27:54 +0400

    QJson is now working
    Remaint work:
    * Icon
    * Trayicon
     Changes to be committed:
    	modified:   qjson/qjson.py

--- qjson/qjson.py
+++ qjson/qjson.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys,os
+import sys,os,simplejson,codecs
 from PyQt4 import QtGui,QtCore,uic
-import simplejson
 
 class MainWin(QtGui.QMainWindow):
 	def __init__(self, parent=None):
@@ -20,25 +19,45 @@ class MainWin(QtGui.QMainWindow):
 	def check(self, int_):
 		if 'Link' in self.ui.tabWidget.tabText(int_):
 			self.table = self.ui.tableLink
+			self.fields = ['link', 'url', 'by']
+			self.defaults = ['', '', 'id']
 		elif 'Control' in self.ui.tabWidget.tabText(int_):
 			self.table = self.ui.tableControl
+			self.fields = ['name', 'value', 'type', 'clear', 'submit']
+			self.defaults = ['', '', 'text', '0', '0']
 		elif 'Error' in self.ui.tabWidget.tabText(int_):
 			self.table = self.ui.tableError
+			self.fields = ['name', 'value', 'ok']
+			self.defaults = ['', '', '0']
 		elif 'Result' in self.ui.tabWidget.tabText(int_):
 			self.table = self.ui.tableResult
+			self.fields = ['name', 'value', 'method']
+			self.defaults = ['', '', 'grep']
 	
 	def write(self):
 		outfile = QtGui.QFileDialog.getSaveFileName()
 		print outfile
 		fh = codecs.open(outfile, mode='a+', encoding='utf-8')
+		valList = []
 		for rindex in range(self.table.rowCount()):
+			val = {}
 			for cindex in range(self.table.columnCount()):
 				try:
-					print self.table.item(rindex, cindex).text()
+					text = str(self.table.item(rindex, cindex).text())
+					if (text == '') and (cindex > 1):
+						text = self.defaults[cindex]
+#					print self.table.item(rindex, cindex).text()
 				except AttributeError:
 					item = QtGui.QTableWidgetItem()
-					item.setText('')
+					item.setText(self.defaults[cindex])
+					text = str(item.text())
 					self.table.setItem(rindex, cindex, item)
+
+				val[self.fields[cindex]] = text
+
+			valList.append(val)
+
+		fh.write(simplejson.dumps(valList))		
 		fh.close()
 
 	def add_row(self):
