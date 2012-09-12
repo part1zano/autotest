e267bfa0a68248d344f95b49f18d54d9bd0fb27e
Maxim V Filimonov <part1zancheg@gmail.com>
2012-09-12 12:59:49 +0400

    Added test_add_news
    Adds news, reads news, reads self-news.
     On branch experimental
     Changes to be committed:
    	new file:   test_add_news.py

---
+++ test_add_news.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from testlib import testcase,myrandom
+import sys
+
+class TestCase(testcase.TestObject):
+	def execute(self):
+		if not testcase.TestObject.execute(self):
+			self.log.write('error', 'login failed')
+			return False
+
+		news_text = myrandom.randomPhrase()
+
+		if not self.edit_control('add_event', news_text, clear=True, click=True):
+			self.log.write('error', 'unable to add news: no edit or error editing')
+			return False
+
+		self.sleep(2)
+
+		if not self.click_btn(u'Отправить'):
+			self.log.write('error', 'no submit btn or error submitting')
+			return False
+		
+		self.log.write('info', 'wrote %s into news' % news_text)
+
+		self.sleep(2)
+
+		if not self.check_div_value('news-view', news_text):
+			self.log.write('error', 'no post in news')
+			return False
+
+		self.log.wirte('info', 'appeared in news...')
+
+		if not self.visit_link('mc_sidebar_profile', 'news'):
+			self.log.write('error', 'error visiting profile')
+			return False
+
+		if not self.check_div_value('news-view', news_text):
+			self.log.write('error', 'no post in self-news')
+			return False
+
+		self.log.write('info', 'appeared in self-news!')
+
+		return True
+
+if __name__ == '__main__':
+	tc = TestCase()
+	sys.exit(int(not tc.execute()))
