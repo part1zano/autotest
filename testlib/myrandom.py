d8cca3fab7482570bfaf0afc7969be5aa3d2c779
Maxim V Filimonov <part1zancheg@gmail.com>
2012-09-12 12:50:00 +0400

    Another one
    And length again
     On branch experimental
     Changes to be committed:
    	modified:   testlib/myrandom.py

--- testlib/myrandom.py
+++ testlib/myrandom.py
@@ -3,6 +3,7 @@ import sys,re,random
 
 phrases = [
 ['The', 'Le', 'Our', 'Their', 'This', 'My', 'Your', 'That', 'Obvious', 'Hilarious', 'Slutty'],
+['quick', 'slow', 'crazy', 'sane', 'lazy', 'hard-working', 'telepathic', 'pathetic', 'apathetic', 'weird', 'unnamed'],
 ['fox', 'dog', 'plane', 'pencil', 'drawer', 'roll', 'dice', 'mother', 'son', 'father', 'daughter'],
 ['is', 'gets', 'likes', 'fakes', 'loves', 'hates', 'dislikes', 'can in', 'cannot in', 'flames', 'changes'],
 ['cake', 'love', 'god', 'math', 'physics', 'orgasm', 'mud', 'dirt', 'bitches', 'whores', 'sluts'],
@@ -12,18 +13,18 @@ phrases = [
 
 
 def randomPhrase(length = len(phrases)):
-phrase = ''
-for index in range(length):
-phrase += ' '+phrases[index][random.randint(0, len(phrases))]
+	phrase = ''
+	for index in range(length):
+		phrase += ' '+phrases[index][random.randint(0, len(phrases))]
 
-return phrase.strip()
+	return phrase.strip()
 
 def randomNum(length):
-number = ''
-for index in range(length):
-number += str(random.randint(0, 9))
+	number = ''
+	for index in range(length):
+		number += str(random.randint(0, 9))
 
-return int(number)
+	return int(number)
 
 def coin():
-return bool(random.randint(0,9) % 2)
+	return bool(random.randint(0,9) % 2)
