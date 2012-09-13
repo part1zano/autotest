cee3da196c46c0bc64f8097494c98f32f250cec6
Maxim V Filimonov <part1zancheg@gmail.com>
2012-09-13 10:24:05 +0400

    Removed some output from myrandom
    No fuckin debug in my scripts
     On branch fix
     Changes to be committed:
    	modified:   testlib/myrandom.py

--- testlib/myrandom.py
+++ testlib/myrandom.py
@@ -15,7 +15,6 @@ phrases = [
 def randomPhrase(length = len(phrases)):
 	phrase = ''
 	for index in range(length):
-		print index
 		rnd_2 = random.randint(0, len(phrases[index])-1)
 		phrase += ' '+phrases[index][random.randint(0, rnd_2)]
 
