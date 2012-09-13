5b510f83b4fcd95f21f7f4118c6d87045092e989
Maxim V Filimonov <part1zancheg@gmail.com>
2012-09-13 18:14:01 +0400

    Work on categories in progress
    Subj. Now it seems to work
     On branch experimental
     Changes to be committed:
    	modified:   test_categories.py

--- test_categories.py
+++ test_categories.py
@@ -22,6 +22,8 @@ class TestCase(testcase.TestObject):
 			self.log.write('error', 'login failed')
 			return False
 
+		self.info['brandName'] = self.get_our_info('brandName')
+
 		for link in self.links:
 			if not self.visit_dlink(link):
 				self.log.write('error', 'error visiting %s' % link['url'])
@@ -37,14 +39,52 @@ class TestCase(testcase.TestObject):
 				deletes.append(cat_item)
 
 		self.log.write('debug', 'found %2d categories' % len(deletes))
+		
+		if len(deletes) > 0:
+			self.log.write('info', 'found categories to delete, deleting')
+			if not self.click_btn(u'Редактировать'):
+				self.log.write('error', 'error clicking edit')
+				return False
 
-		if not self.click_btn(u'Редактировать'):
-			self.log.write('error', 'error clicking edit')
-			return False
+			for delete in deletes:
+				if not self.click_btn('//label[@for="%s"]' % delete['id'], by='xpath'):
+					self.log.write('error', 'error clicking %s checkbox' % delete['id'])
+					return False
+				self.log.write('debug', 'submitted %s checkbox' % delete['id'])
 
-		for delete in deletes:
+			if not self.click_btn(u'Сохранить'):
+				self.log.write('error', 'error submitting')
+				return False
+			self.log.write('info', 'deleted \'em all')
 
+		for category in categories:
+			if not self.click_btn(u'Редактировать'):
+				self.log.write('error', 'error clicking edit for normal edit of category %s' % category['id'])
+				return False
 
+			if not self.click_btn('//label[@for="%s"]' % category['id'], by='xpath'):
+				self.log.write('error', 'error clicking checkbox for %s' % category['id'])
+				return False
+
+			if not self.click_btn(u'Сохранить'):
+				self.log.write('error', 'error submitting change for %s category' % category['id'])
+				return False
+
+			self.sleep(2)
+
+			if not self.visit_link(category['text'], 'q', by='text'):
+				self.log.write('error', 'error visiting search')
+				return False
+
+			if not self.find_link(self.info['brandName'], by='text'):
+				self.log.write('error', 'brandName not found in search for category %s' % category['id'])
+				return False
+
+			self.driver.back()
+			self.sleep(2)
+			if not self.check_div('profile'):
+				self.log.write('error', 'error returning to profile')
+				return False
 
 		return True
 
