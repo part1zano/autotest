cef00f41aca29d90b31621924a4f571da5524024
Maxim V Filimonov <part1zancheg@gmail.com>
2012-09-18 12:53:02 +0400

    Added random_login, random_email, random_domain
    Now I can generate that shit easily
     On branch experimental
     Changes to be committed:
    	modified:   testlib/myrandom.py

--- testlib/myrandom.py
+++ testlib/myrandom.py
@@ -11,6 +11,36 @@ phrases = [
 ['plague', 'shadow', 'shade', 'the dark', 'the box', 'bed', 'shower', 'work', 'town', 'city', 'village']
 ]
 
+alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+
+def random_login(words=3, separator='_'):
+	login = ''
+	for i in range(words):
+		index = random.randint(1, len(phrases)-1)
+		login += re.sub(' ', separator, phrases[index][random.randint(0, len(phrases[index])-1)])+separator
+
+	login = re.sub(separator+'$', '', login)
+	
+	return login
+
+def random_1lvl_domain(length=2):
+	domain = ''
+	for i in range(length):
+		domain += alphabet[random.randint(0, len(alphabet)-1)]
+	
+	return domain
+
+def random_domain(lvl=2, lvl1_length=2):
+	domain = ''
+	for i in range(lvl-1):
+		index = random.randint(1, len(phrases)-1)
+		domain += re.sub(' ', '-', phrases[index][random.randint(0, len(phrases[index])-1)])+'.'
+
+	domain += random_1lvl_domain(lvl1_length)
+	return domain
+
+def random_email(login_len=3, login_separator='_', domain_lvl=2, lvl1_length=2):
+	return random_login(login_len, login_separator)+'@'+random_domain(domain_lvl, lvl1_length)
 
 def randomPhrase(length = len(phrases)):
 	phrase = ''
