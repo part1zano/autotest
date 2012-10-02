42fd9f25fdabef071c36d6b4ac692c3d0c10d6d5
Maxim V Filimonov <part1zancheg@gmail.com>
2012-10-02 17:35:43 +0400

    Fxd test_titles a bit
    Now it sure works
     On branch experimental
     Changes to be committed:
    	modified:   json_lists/titles/titles.json
    	modified:   test_titles.py

--- test_titles.py
+++ test_titles.py
@@ -48,6 +48,7 @@ class TestCase(testcase.TestObject):
 				self.log.write('error', 'wrong title for url %s, see above' % link['url'])
 				return False
 
+		self.log.write('info', '%s passed' % sys.argv[0])
 		return True
 
 if __name__ == '__main__':
