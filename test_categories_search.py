6b86027ebab65ee0e28a0ff0fd52dd199396a1a0
Maxim V Filimonov <part1zancheg@gmail.com>
2012-10-02 15:40:45 +0400

    Now categories_search rly works
    Subj.
     On branch experimental
     Changes to be committed:
    	modified:   test_categories_search.py

--- test_categories_search.py
+++ test_categories_search.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 
+from testlib import testcase
 import test_categories
 import sys
 
@@ -15,22 +16,51 @@ class TestCase(test_categories.TestCase):
 			return False
 
 		for link in self.links:
-			if not self.visit_link(link):
+			if not self.visit_dlink(link):
 				self.log.write('error', 'error visiting %s, see above' % link['url'])
 				return False
 
 		inserts = []
 
-		for ccat, name in categories.items():
-			if not self.find_link(name, by='text'):
-				toInsert.append({ccat: name})
+		for category in self.categories:
+			if not self.find_link(category['text'], by='text'):
+				inserts.append(category)
 
-		for category in toInsert:
+		if len(inserts) > 0:
 			if not self.click_btn(u'Редактировать', by='text'):
 				self.log.write('error', 'some shit pressing edit btn, see above')
 				return False
 
+		for category in inserts:
+			if not self.click_btn('//label[@for="%s"]' % category['id'], by='xpath'):
+				self.log.write('error', 'no such checkbox in profile-edit: %s' % category['id'])
+				return False
 
+		if len(inserts) > 0:
+			if not self.click_btn(u'Сохранить'):
+				self.log.write('error', 'error submitting info')
+				return False
+
+		if not self.visit_link(u'Компании', 'search', by='text'):
+			self.log.write('error', 'error visiting search')
+			return False
+		
+		ids = [(0,1), (0,2), (1,2), (0,1,2)]
+
+		for id_tuple in ids:
+			for id_ in id_tuple:
+				if not self.click_btn('//label[@for="%s"]' % self.categories[id_]['id'], by='xpath'):
+					self.log.write('error', 'error clicking %s checkbox' % self.categories[id_]['id'])
+					return False
+
+			self.sleep(2)
+
+			if not self.find_link(self.info['brandName'], count=2, by='text'):
+				try:
+					self.log.write('error', 'brandName not found in set %d, %d' % (id_tuple)) # FIXME :: HUGE UGLY PIECE OF DOG-NAIL
+				except TypeError:
+					self.log.write('error', 'brandName not found in set %d, %d, %d' % (id_tuple)) # FIXME :: HUGE UGLY PIECE OF DOG-NAIL
+				return False
 
 		self.log.write('info', '%s PASSED' % sys.argv[0])
 		return True
