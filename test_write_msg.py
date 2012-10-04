30fc4f7726202e9b4237b4bb751103dec328922b
Maxim V Filimonov <part1zancheg@gmail.com>
2012-10-04 10:22:25 +0400

    Now write-msg test should work
    Subj
     On branch experimental
     Your branch is ahead of 'origin/experimental' by 1 commit.
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    	modified:   test_write_msg.py
    	modified:   testlib/testcase.py

--- test_write_msg.py
+++ test_write_msg.py
@@ -17,23 +17,40 @@ class TestCase(testcase.TestObject):
 				self.log.write('error', 'error visiting %s, see above' % link['url'])
 				return False
 
-			if not self.edit_control('chat_message_ta', msg, ctl_type='text', by='id'):
-				self.log.write('error', 'error editing message textarea')
-				return False
-			self.log.write('debug', 'entered msg successfully')
-			self.log.write('debug', 'msg is: %s' % msg)
+		if not self.edit_control('chat_message_ta', msg, ctl_type='text', by='id'):
+			self.log.write('error', 'error editing message textarea')
+			return False
+		self.log.write('debug', 'entered msg successfully')
+		self.log.write('debug', 'msg is: %s' % msg)
 
-			if not self.click_btn('send_message_btn', by='id'):
-				self.log.write('error', 'error submitting msg')
-				return False
+		if not self.click_btn('send_message_btn', by='id'):
+			self.log.write('error', 'error submitting msg')
+			return False
 
-			self.log.write('debug', 'submitted entered msg successfully')
+		self.log.write('debug', 'submitted entered msg successfully')
 
-			self.sleep(2)
+		self.sleep(2)
 
+		if not self.check_div_value('//ul[@class="messages"]', msg):
+			self.log.write('error', 'no msg appeared: NOK')
+			return False
 
+		self.log.write('info', 'sent msg successfully')
 
-			return True
+		return True
+
+	def check_appeared_msg(self, msg):
+		if not self.visit_link('main-header-chat-notification', 'chat', by='id'):
+			self.log.write('error', 'not going to chat, see above')
+			return False
+
+		if not self.check_div_value('//ul[@class="messages"]', msg):
+			self.log.write('error', 'msg not received or wrong something')
+			return False
+
+		self.log.write('info', 'received msg successfully')
+		return True
+
 
 	def execute(self):
 #		asLogin = self.aslogin
@@ -42,6 +59,22 @@ class TestCase(testcase.TestObject):
 		if not testcase.TestObject.execute(self):
 			self.log.write('error', 'login failed')
 			return False
+
+		msg = myrandom.random_phrase()
+		if not self.send_msg_by_title(u'™', msg):
+			self.log.write('error', 'error sending msg, see above')
+			return False
+
+		self.login, self.password, self.aslogin, self.aspwd = self.aslogin, self.aspwd, self.login, self.password
+		if not self.do_login():
+			self.log.write('error', 'error logging in as %s' % self.aslogin)
+			return False
+
+		if not self.check_appeared_msg(msg):
+			self.log.write('error', 'msg not received, see above')
+			return False
+
+		self.log.write('info', '%s PASSED' % sys.argv[0])
 
 		return True
 
