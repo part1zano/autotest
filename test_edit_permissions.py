8072757fde1d3a533108c7cce21e7bd2f3223e50
Maxim V Filimonov <part1zancheg@gmail.com>
2012-10-19 16:01:38 +0400

    Work on perms in progress
    Subj
     On branch experimental
     Your branch is ahead of 'origin/experimental' by 2 commits.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   test_edit_permissions.py

--- test_edit_permissions.py
+++ test_edit_permissions.py
@@ -2,13 +2,31 @@
 
 from testlib import testcase,myrandom
 import sys
+import test_recommend, test_contragent
 
 class TestCase(testcase.TestObject):
 	def check_permission(self, perm, value):
 		if 'news' in perm:
 			links = self.make_json_list('json_lists/permissions/news.json')
+
+			for link in links:
+				if not self.visit_dlink(link):
+					self.log.write('error', 'error visiting %s' % link['url'])
+					return False
+
+			post = myrandom.random_phrase()
+
+			if self.edit_control('add_event', post, 'text', click=True) != value:
+				self.log.write('error', 'wrong permission set for %s permission' % perm)
+				return False
+
 		elif 'contractors' in perm:
-			pass
+			links = self.make_json_list('json_lists/permissions/contractors.json')
+			for link in links:
+				if not self.visit_dlink(link):
+					self.log.write('error', 'error visiting %s' % link['url'])
+					return False
+
 		elif 'recommend' in perm:
 			pass
 		elif 'finance' in perm:
@@ -18,8 +36,11 @@ class TestCase(testcase.TestObject):
 		else:
 			self.log.write('error', 'unknown permission type')
 			return False
+
 		return True
+
 	def execute(self):
+		self.title_fragment = u'»'
 		self.links = self.make_json_list('json_lists/permissions/links.json')
 		if not testcase.TestObject.execute(self):
 			self.log.write('error', 'login failed')
