a10f4d6e38cbbc8233a6ae3b5afc19dc9c26edc6
Maxim V Filimonov <part1zancheg@gmail.com>
2012-10-31 18:02:01 +0400

    Invite test (simple as fuck) is fully functional
    Subj
     On branch experimental
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   test_invite.py

--- test_invite.py
+++ test_invite.py
@@ -9,6 +9,23 @@ class TestCase(testcase.TestObject):
 			self.log.write('error', 'login failed')
 			return False
 
+		self.info['uid'] = self.json_info()['common_data']['ownEmployee']['_id']
+		to_detect = self.url+'/invites/from-person/'+self.info['uid']
+
+		for link in self.make_json_list('json_lists/invite/links.json'):
+			if not self.visit_dlink(link, sleep=True):
+				self.log.write('error', 'error visiting %s' % link['url'])
+				return False
+
+		ta_value = self.get_value('textarea-message', by='id')
+		if ta_value is None:
+			self.log.write('error', 'error getting msg value')
+			return False
+
+		if to_detect not in ta_value:
+			self.log.write('error', 'url not in textarea')
+			return False
+
 		self.log.write('info', '%s PASSED' % sys.argv[0])
 		return True
 
