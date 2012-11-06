3802875ba6587c9e6f95d4301c063ef45a951a09
Maxim V Filimonov <part1zancheg@gmail.com>
2012-11-06 10:29:15 +0400

    Added even moar sleeps
    Subj
     On branch experimental
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   test_edit_permissions.py
    	modified:   test_request_recommendation.py

--- test_edit_permissions.py
+++ test_edit_permissions.py
@@ -14,7 +14,7 @@ class TestCase(testcase.TestObject):
 			links = self.make_json_list('json_lists/permissions/news.json')
 
 			for link in links:
-				if not self.visit_dlink(link):
+				if not self.visit_dlink(link, sleep=True):
 					self.log.write('error', 'error visiting %s' % link['url'], sleep=True)
 					return False
 
